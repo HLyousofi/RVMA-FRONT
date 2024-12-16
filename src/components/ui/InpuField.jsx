c61322d2b06d0f53bdf632eaec7dae4853bbd6fd
Hlyousofi <hlyousofi@gmail.com>
2024-12-16 20:08:58 +0100

    create InpuField component

---
+++ src/components/ui/InpuField.jsx
@@ -0,0 +1,38 @@
+// src/components/InputField.jsx
+
+import { Controller } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+
+const InputField = ({ 
+  name, 
+  label, 
+  control, 
+  defaultValue = '', 
+  rules = {}, 
+  type = 'text', 
+  ...rest 
+}) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      rules={rules}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          label={label}
+          type={type}
+          variant="outlined"
+          fullWidth
+          error={!!error}
+          helperText={error?.message}
+          className="mb-4"
+          {...rest}
+        />
+      )}
+    />
+  );
+};
+
+export default InputField;
